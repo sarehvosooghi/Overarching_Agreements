f34100ef1499850255d3e605b8d0cc432e867a31
sarehvosooghi <sareh.vosooghi@kuleuven.be>
2026-09-11 09:58:11 +0200

    Delete src/sareh_project directory

--- src/sareh_project/model/emission_abatement.py
+++
@@ -1,9 +0,0 @@
-# emission_abatement.py
-
-import numpy as np
-from . import constants as C
-
-def emission_abatement(M: np.ndarray) -> np.ndarray:
-    # q*(m) = m * β_E * η / (1 − β_E * ϕ)
-    m = M[:, 0].astype(float)
-    return m * C.Beta_E * C.eta / (1.0 - C.Beta_E * C.phi)
